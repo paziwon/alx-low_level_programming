a6485e9f914ccce1780a8bdecefd0650c35306e1
paziwon <97870585+paziwon@users.noreply.github.com>
2022-12-08 18:54:59 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * mian - Entry
- * Return: Always 0
- */
+* main. -entry
+* return: Always 0
+*/
 int main(void)
-{
-   puts("Programming is like building a multilingual puzzle\n");
-   return (0);
-}
+{    puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+}     
