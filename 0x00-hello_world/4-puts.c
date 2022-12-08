3c063a3ba11812a4fe364fa30a8f190916ae189d
paziwon <paziwonze@gmail.com>
2022-12-08 13:13:19 -0800

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
-* mian - Entry
-* Return: Always 0
-*/
+ * main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0;
-
-}     
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
