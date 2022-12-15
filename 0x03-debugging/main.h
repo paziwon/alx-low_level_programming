2888f9ce5d3ed413efa660df3643eff54052847d
paziwon <paziwonze@gmail.com>
2022-12-15 07:58:34 -0800

    yes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,34 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void positive_or_negative(int i);
 
-#endif /* MAIN_H */
-carrie@ubuntu:/debugging$
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not o\n", n, a); 
+	}
+	return (0);
+}
